bfe36f29492139b8e19a64c853891705a0876ee9
unknown <phoopwintaung15301@gmail.com>
2024-02-08 01:41:58 +0630

    first commit

--- js/custom.js
+++ js/custom.js
@@ -56,3 +56,14 @@
 
 })(jQuery);
 
+$("a b").html(function(index, html) {
+  return html.replace(/\S/g, '<span>$&</span>');
+});
+
+$("a").click(function(){
+        $("a").addClass("loading");     
+        setTimeout(function(){
+          $("a").removeClass("loading"); 
+        }, 8000);
+
+});
